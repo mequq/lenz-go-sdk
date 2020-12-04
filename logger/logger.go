351dbc587de7e994991628e1c46d21f2b590a647
MAK <mak@localhost.localdomain>
2020-12-04 12:18:30 +0330

    add level for logger

--- logger/logger.go
+++ logger/logger.go
@@ -16,6 +16,19 @@ func init() {
 	godotenv.Load()
 	zerolog.TimeFieldFormat = zerolog.TimeFormatUnixMicro
 
+	loggerLevelStr := os.Getenv("LOGGER_LEVEL")
+	loggerLevel := zerolog.InfoLevel
+
+	if loggerLevelStr == "DEBUG" {
+		loggerLevel = zerolog.DebugLevel
+	} else if loggerLevelStr == "INFO" {
+		loggerLevel = zerolog.InfoLevel
+	} else if loggerLevelStr == "WARNING" {
+		loggerLevel = zerolog.WarnLevel
+	} else if loggerLevelStr == "ERROR" {
+		loggerLevel = zerolog.ErrorLevel
+	}
+
 	// logFile, err := os.OpenFile(os.Getenv("LOGGER_FILE"), os.O_CREATE|os.O_APPEND|os.O_RDWR, 0666)
 	// if err != nil {
 	// 	panic(err)
@@ -28,7 +41,7 @@ func init() {
 	// consoleWriter := zerolog.ConsoleWriter{Out: os.Stdout}
 
 	multi := zerolog.MultiLevelWriter(con)
-	Logger = zerolog.New(multi).With().
+	Logger = zerolog.New(multi).Level(loggerLevel).With().
 		Array("tags", zerolog.Arr().Str(os.Getenv("MS_NAME"))).
 		Timestamp().Logger()
 }
