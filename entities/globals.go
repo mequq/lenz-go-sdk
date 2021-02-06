98f98268972fcf1e35f2af48320cb96458db6702
MAK <mak@localhost.localdomain>
2021-02-06 18:03:04 +0330

    change Fake-X-Forwarded-For to X-Forwarded-For

--- entities/globals.go
+++ entities/globals.go
@@ -3,4 +3,4 @@ package entities
 // XForwardedForKey use for get X-Forwarded-For header
 // it is change to variable because in some case like performance test or stress test
 // we should change it to Fake-X-Forwarded-For
-var XForwardedForKey string = "Fake-X-Forwarded-For"
+var XForwardedForKey string = "X-Forwarded-For"
